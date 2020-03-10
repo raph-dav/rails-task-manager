19ceed80735d4bc2c2efc41e13ac9756448d5cfc
Raphael Daverio <raphael.daverio@gmail.com>
2020-03-10 15:30:40 +0100

    created an app to add tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,28 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # READ ALL THE TASK
+  get '/tasks', to: 'tasks#index', as: :tasks
+
+  # CREATE A TASK >> 2 REQUESTS
+  # 1 DISPLAY THE FORM --> New
+  get '/tasks/new', to: 'tasks#new'
+
+  # 2 SAVE IN DB
+  post '/tasks', to: 'tasks#create'
+
+  # READ ONE TASK
+  get '/tasks/:id', to: 'tasks#show', as: :task
+
+
+  # UPDATE A TASK >> 2 REQUESTS
+  # 1 DISPLAY THE FORM ALREADY FILLED IN - GET
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :task_edit
+  # 2 POST PARAMS & UPDATE TO THE DB
+  patch '/tasks/:id', to: 'tasks#update'
+
+  # #DELETE A TASK
+  delete '/tasks/:id', to: 'tasks#destroy'
+  # delete '/restaurants/:id', to: "restaurants#destroy"
+
 end
